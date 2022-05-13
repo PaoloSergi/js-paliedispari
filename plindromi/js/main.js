fdb32b28d8c901eee1e931341d00b43b5d8720dd
PaoloGit <paolo.sergi.88@gmail.com>
2022-05-13 06:34:43 +0200

    palindroma fatto

---
+++ plindromi/js/main.js
@@ -0,0 +1,22 @@
+// Chiedere all’utente di inserire una parola
+// Creare una funzione per capire se la parola inserita è palindroma
+
+let wordTocheck = prompt("Inserisci una parola");
+
+console.log(palindromeCheck(wordTocheck));
+
+
+
+
+function palindromeCheck(word) {
+    let flag = 0;
+
+    for (let i=0; i<(word.length)/2;i++){
+        if (word.charAt(i)!=word.charAt((word.length -1) - i)){
+            flag += 1;
+        }
+    }
+    if (flag===0){
+        return `La parola ${word} è palindroma`
+    } return `La parola ${word} non è palindroma`
+}
